b2014511ce9e97cfa3a6a76ef611cb3a0591e5b4
alvhidrod <alvaro.hidalgo.rodriguez12@gmail.com>
2024-02-25 20:53:22 +0100

    Creacion de modelo reseña

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Review(models.Model):
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(0)])
+    worker = models.OneToOneField(User, on_delete= models.CASCADE)
+    description = models.TextField(max_length= 1000)
+    date = models.DateField()
