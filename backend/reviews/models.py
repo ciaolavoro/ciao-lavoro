d189238228351434c6f68554fd3bd5db01c76344
alvhidrod <alvaro.hidalgo.rodriguez12@gmail.com>
2024-02-27 10:46:59 +0100

    Cambio del import

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from backend.account.models import Account
+from account.models import Account
 
 class Review(models.Model):
     author = models.OneToOneField(Account, on_delete=models.CASCADE, related_name = 'author_review')
