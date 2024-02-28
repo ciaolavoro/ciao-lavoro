95607172b693b0b7f83ffc1cf7758993f289c859
Adrián García Chavero <adrgarcha@alum.us.es>
2024-02-28 20:00:59 +0100

    Tarea 5 - Mejorar el enrutado de páginas #41

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -1,5 +1,3 @@
-import Navbar from "../Navbar";
-import Background from "../Background";
 import DescriptionCard from "./DescriptionCard";
 import BriefcaseIcon from "../icons/BriefcaseIcon";
 import EuroIcon from "../icons/EuroIcon";
@@ -8,32 +6,28 @@ import BankNotesIcon from "../icons/BankNotesIcon";
 export default function Home() {
 
   return (
-    <div className="relative font-sans">
-      <Navbar />
-      <Background />
-      <main className="h-screen">
-        <section>
-          <form className="flex justify-center gap-2 my-4">
-            <input type="text" placeholder="Buscar servicios de trabajadores" className="w-96 pl-2 border rounded" />
-            <button type="submit" className="bg-orange-300 rounded px-3 py-1 font-semibold">Buscar</button>
-          </form>
-          <img src="./src/assets/home/home-banner.jpg" alt="Banner de la página de inicio" className="w-full h-[390px] px-28 object-cover" />
-        </section>
-        <section className="flex justify-center gap-4 px-32 py-6">
-          <DescriptionCard icon={<BriefcaseIcon />} title="Encuentre los mejores trabajadores">
-            Descubre una amplia gama de profesionales de diversos campos.
-            Nuestra plataforma te conecta con trabajadores cualificados que están listos para aportar su experiencia a tus necesidades.
-          </DescriptionCard>
-          <DescriptionCard icon={<EuroIcon />} title="Consiga las mejores ofertas">
-            Nos aseguramos de que obtengas el máximo valor por tu dinero.
-            Nuestra plataforma te permite comparar tarifas y elegir servicios que se ajusten a tu presupuesto sin comprometer la calidad.
-          </DescriptionCard>
-          <DescriptionCard icon={<BankNotesIcon />} title="Obtenga dinero fácilmente">
-            ¿Eres un profesional en busca de trabajo? Únete a nuestra plataforma y accede a numerosas oportunidades de trabajo.
-            Establece tus tarifas, elige tus proyectos y comienza a ganar.
-          </DescriptionCard>
-        </section>
-      </main>
-    </div>
+    <>
+      <section>
+        <form className="flex justify-center gap-2 my-4">
+          <input type="text" placeholder="Buscar servicios de trabajadores" className="w-96 pl-2 border rounded" />
+          <button type="submit" className="bg-orange-300 rounded px-3 py-1 font-semibold">Buscar</button>
+        </form>
+        <img src="./src/assets/home/home-banner.jpg" alt="Banner de la página de inicio" className="w-full h-[390px] px-28 object-cover" />
+      </section>
+      <section className="flex justify-center gap-4 px-32 py-6">
+        <DescriptionCard icon={<BriefcaseIcon />} title="Encuentre los mejores trabajadores">
+          Descubre una amplia gama de profesionales de diversos campos.
+          Nuestra plataforma te conecta con trabajadores cualificados que están listos para aportar su experiencia a tus necesidades.
+        </DescriptionCard>
+        <DescriptionCard icon={<EuroIcon />} title="Consiga las mejores ofertas">
+          Nos aseguramos de que obtengas el máximo valor por tu dinero.
+          Nuestra plataforma te permite comparar tarifas y elegir servicios que se ajusten a tu presupuesto sin comprometer la calidad.
+        </DescriptionCard>
+        <DescriptionCard icon={<BankNotesIcon />} title="Obtenga dinero fácilmente">
+          ¿Eres un profesional en busca de trabajo? Únete a nuestra plataforma y accede a numerosas oportunidades de trabajo.
+          Establece tus tarifas, elige tus proyectos y comienza a ganar.
+        </DescriptionCard>
+      </section>
+    </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,32 +9,39 @@ import Services from './components/search/Services.jsx'
 import Profile from './components/user/Profile.jsx'
 import Chat from './components/chat/Chat.jsx'
 import ErrorPage from './components/ErrorPage.jsx'
+import Root from './components/Root.jsx'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <Root />,
     errorElement: <ErrorPage />,
-  },
-  {
-    path: '/auth',
-    element: <Login />,
-  },
-  {
-    path: '/about',
-    element: <About />,
-  },
-  {
-    path: '/search',
-    element: <Services />,
-  },
-  {
-    path: '/profile/:id',
-    element: <Profile />,
-  },
-  {
-    path: '/chat',
-    element: <Chat />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'auth',
+        element: <Login />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'search',
+        element: <Services />,
+      },
+      {
+        path: 'profile/:id',
+        element: <Profile />,
+      },
+      {
+        path: 'chat',
+        element: <Chat />,
+      },
+    ]
   },
 ]);
 
