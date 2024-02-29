8fe02a23491bf0d5377613ef96e9adbbd230e158
fracamcam <fracamcam@alum.us.es>
2024-02-29 16:35:03 +0100

    Tarea 4 - Vista de Inicio de sesión. #40
    Se ha añadido la vista de inicio de sesión.

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,6 +1,59 @@
-export default function Login() {
-    return (
-        <>
-        </>
-    )
-}
+import { useState } from 'react';
+import { NavLink } from "react-router-dom"
+import Background from "../Background";
+
+const LoginPage = () => {
+ const [email, setEmail] = useState('');
+ const [password, setPassword] = useState('');
+
+ const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle login logic here
+ };
+
+ return (
+    <div className="relative font-sans">
+      <Background/>
+      <main className="flex flex-col items-center justify-center min-h-screen p-8 bg-marron-200">
+          <NavLink to="/">
+            <img src="ciaolavoro-logo.png" alt="Logo de CiaoLavoro" className='w-24 h-auto mb-8'/>
+          </NavLink>
+        <div className="flex flex-col items-center bg-white p-8 rounded-lg shadow-md">
+          <form onSubmit={handleSubmit} className="flex flex-col items-center">
+            <label className="mb-4">
+              Usuario/Email:
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                placeholder='Usuario/Email'
+              />
+            </label>
+            <label className="mb-4">
+              Contraseña:
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="w-full p-2 mb-4 border border-gray-300 rounded"
+                placeholder='Contraseña'
+              />
+            </label>
+            <center>
+              <div>
+                <button type="submit" className="p-2 bg-orange-400 text-black rounded cursor-pointer font-inherit border-none">Iniciar sesión</button>
+              </div>
+              <br></br>
+              
+              <h3>¿Aún no estás registrado?</h3><NavLink to="/register" className="text-orange-400 underline">Crear nueva cuenta</NavLink>
+            </center>
+          </form>
+        </div>
+      </main>
+    </div>
+ );
+};
+
+export default LoginPage;
