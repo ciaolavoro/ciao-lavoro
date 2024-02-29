d727036273ee5c591b3f0b9ed9bdbef95b5b3b83
Adrián García Chavero <adrgarcha@alum.us.es>
2024-02-29 21:57:49 +0100

    Tarea 11 - Actualizar estilos e imagen de la vista #47

--- frontend/src/components/service/Service.jsx
+++ frontend/src/components/service/Service.jsx
@@ -1,31 +1,28 @@
+import talonFlame from "../../assets/service/talonflame.jpg";
 
-
-// Muestra la vista de la oferta
 export default function Service() {
+
   const service = {
-    photo: "./src/assets/home/talonflame.jpg",
+    photo: {talonFlame},
     name: "Talonflame",
     profession: "Bird",
     city: "Ciudad Luminalia",
     experience: "37",
     tasks: "Puedo volar de un lado a otro y comer pan",
     language: "Español, Inglés, Pajaro"
-
   }
-  return (
-    //Se ha usado IA para las partes relativas al uso de tailwind en este codigo
 
-    <div className="max-w-md mx-auto bg-white shadow-md rounded-lg overflow-hidden p-6">
+  return (
+    <div className="max-w-md mx-auto my-6 bg-white border rounded-lg overflow-hidden p-6">
       <h2 className="text-2xl font-semibold mb-4">Detalles del servicio</h2>
-      <img class="rounded-full w-96 h-96" src={service.photo} alt="foto del trabajador"></img>
-      <p className="mb-2"><strong>Nombre del ofertante:</strong> {service.name}</p>
+      <img className="mx-auto border rounded-full w-72 object-cover" src={service.photo.talonFlame} alt="Foto del trabajador"></img>
+      <p className="mb-2 mt-4"><strong>Nombre del ofertante:</strong> {service.name}</p>
       <p className="mb-2"><strong>Profesión:</strong> {service.profession}</p>
       <p className="mb-2"><strong>Ciudad:</strong> {service.city}</p>
       <p className="mb-2"><strong>Experiencia:</strong> {service.experience}</p>
       <p className="mb-2"><strong>Tareas:</strong> {service.tasks}</p>
       <p className="mb-2"><strong>Idioma:</strong> {service.language}</p>
     </div>
-
   );
 }
 
