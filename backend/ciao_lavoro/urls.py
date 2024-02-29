e0157e91b0c6ddfabe084b17bf8079c8e36cd579
JoseLuisCobo17 <joscobari@alum.us.es>
2024-02-29 18:11:26 +0100

    Tarea 3: Lógica usuario Login #39
    Finalización de la tarea

--- backend/ciao_lavoro/urls.py
+++ backend/ciao_lavoro/urls.py
@@ -17,20 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-
 from api import views
 
-
-
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'user', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('user/', include('users.urls', namespace='users')),
+    path('user/', include('user.urls', namespace='user')),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls'))
 ]
-
-urlpatterns += router.urls
