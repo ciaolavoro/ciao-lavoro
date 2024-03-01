8a26954745b48f3660a89f3eac6dc634286f0f95
Samalbalat <saoalbalat@gmail.com>
2024-03-01 17:01:49 +0100

    Task 7 - Lógica creación Servicio #43
    Corrección de URL
    El problema del POST radica en la URL dado que cambiandola se puede hacer post

--- backend/service/urls.py
+++ backend/service/urls.py
@@ -4,7 +4,7 @@ from .views import ServiceList, ServiceCreation, JobCreation
 app_name = 'service'
 
 urlpatterns = [
-    path('services/', ServiceList.as_view(), name='service-list'),
+    path('', ServiceList.as_view(), name='service-list'),
     path('create/', ServiceCreation.as_view(), name='service-create'),
-     path('create/job/', JobCreation.as_view(), name='job-create')
+    path('create/job/', JobCreation.as_view(), name='service-job-create'),
 ]
