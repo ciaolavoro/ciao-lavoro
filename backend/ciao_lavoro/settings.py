ea05378a05d12cd3dacc0cf13d9d421d5602d0e8
Adrián García Chavero <adrgarcha@alum.us.es>
2024-03-03 12:19:31 +0100

    Task 1 - Modificar los hosts autorizados #37

--- backend/ciao_lavoro/settings.py
+++ backend/ciao_lavoro/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    env('DJANGO_ALLOWED_HOST'),
+]
 
 
 # Application definition
