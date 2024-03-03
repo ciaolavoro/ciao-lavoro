e68e760dc7af04a17f08a6ee96281d21d432e82a
pabcueper <pabcueper@alum.us.es>
2024-03-03 11:02:02 +0100

    Fix Task 23 - Corrección errores Codacy.

--- backend/contract/views.py
+++ backend/contract/views.py
@@ -1,7 +1,6 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Contract
 from api.serializers import ContractSerializer
 
 class ContractCreation(APIView):
