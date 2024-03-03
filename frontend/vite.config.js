c9bc1d02368cf9f0720b80042337e630004e883c
Adrián García Chavero <adrgarcha@alum.us.es>
2024-03-03 11:12:24 +0100

    Task 1 - Variable de entorno en Frontend #37

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default () => {
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
   const config = {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:8000/',   // poner url nueva de despliegue de backend
+          target: env.BACKEND_API_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
