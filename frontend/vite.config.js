4a361c386d009601e9accd6093c6687a0465b647
Adrián García Chavero <adrgarcha@alum.us.es>
2024-03-03 14:29:57 +0100

    Task 1 - Cambiar configuracion de Vite #37

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default () => {
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
   const config = {
     plugins: [react()],
     server: {
       proxy: {
         '/api': {
-          target: 'https://ciao-lavoro-l7qa.onrender.com/',
+          target: env.BACKEND_API_URL,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
