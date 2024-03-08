6ec121789c8fbb8486e637952d3dc6e8c6d3cbcb
fracamcam <fracamcam@alum.us.es>
2024-03-08 12:25:53 +0100

    Task 2: Creación registro de usuarios (frontend)

--- frontend/src/components/register/Register.jsx
+++ frontend/src/components/register/Register.jsx
@@ -5,19 +5,13 @@ import defaultImage from './imagen.png';
 
 const RegisterPage = () => {
  const [username, setUsername] = useState('');
- const [fullName, setFullName] = useState('');
+ const [name, setName] = useState('');
+ const [lastName, setLastName] = useState('');
  const [email, setEmail] = useState('');
  const [password, setPassword] = useState('');
  const [confirmPassword] = useState('');
  const [image, setImage] = useState(defaultImage);
- const [nif, setNif] = useState('');
- const [phoneNumber, setPhoneNumber] = useState('');
- const [description, setDescription] = useState('');
- const [isWorker, setIsWorker] = useState(false);
- const [profession, setProfession] = useState('');
- const [experience, setExperience] = useState('');
- const [degree, setDegree] = useState('');
- const [motherLanguage, setMotherLanguage] = useState('');
+ const [birthdate, setBirthdate] = useState('');
 
  const handleSubmit = (event) => {
     event.preventDefault();
@@ -48,12 +42,39 @@ const RegisterPage = () => {
         <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-xl">
           <form onSubmit={handleSubmit} className="flex flex-col items-center space-y-4">
             <div className="flex flex-wrap justify-between w-full">
-              <div className="w-1/3 pr-4">
+            <div className="w-full flex justify-center">
+              <label htmlFor="image-upload" className="block">
+                <div className="bg-green-600 text-black px-4 py-2 rounded-lg cursor-pointer" style={{ width: '150px', height: '60px', fontSize: '16px', alignItems:'center', display:'flex', justifyContent:'center' }}>
+                 Selecciona tu foto de perfil
+                 <input
+                    type="file"
+                    id="image-upload"
+                    onChange={handleImageChange}
+                    required
+                    style={{ display: 'none' }}
+                 />
+                </div>
+              </label>
+            </div>
+            
+              {/* Columna 1: Datos del usuario */}
+              <div className="w-1/2 pr-4">
+              <br></br>
                 <label className="block">
                  <input
                     type="text"
                     value={username}
                     onChange={(e) => setUsername(e.target.value)}
+                    required
+                    placeholder='Nombre de usuario'
+                    className="w-full p-2 mb-4 border border-gray-300 rounded-md"
+                 />
+                </label>
+                <label className="block">
+                 <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
                     required
                     placeholder='Nombre'
                     className="w-full p-2 mb-4 border border-gray-300 rounded-md"
@@ -62,10 +83,24 @@ const RegisterPage = () => {
                 <label className="block">
                  <input
                     type="text"
-                    value={fullName}
-                    onChange={(e) => setFullName(e.target.value)}
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
                     required
                     placeholder='Apellidos'
+                    className="w-full p-2 mb-4 border border-gray-300 rounded-md"
+                 />
+                </label>
+              </div>
+              {/* Columna 2: Datos de la cuenta */}
+              <div className="w-1/2 pr-4">
+              <br></br>
+                <label className="block">
+                 <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    placeholder='Contraseña'
                     className="w-full p-2 mb-4 border border-gray-300 rounded-md"
                  />
                 </label>
@@ -81,107 +116,14 @@ const RegisterPage = () => {
                 </label>
                 <label className="block">
                  <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    type="date"
+                    value={birthdate}
+                    onChange={(e) => setBirthdate(e.target.value)}
                     required
-                    placeholder='Contraseña'
+                    placeholder='Fecha de nacimiento'
                     className="w-full p-2 mb-4 border border-gray-300 rounded-md"
                  />
                 </label>
-              </div>
-              <div className="w-1/3 pr-4">
-                <label htmlFor="image-upload" className="block">
-                 <div className="bg-green-600 text-black px-4 py-2 rounded-lg cursor-pointer" style={{ width: '150px', height: '60px', fontSize: '16px', alignItems:'center', display:'flex', justifyContent:'center' }}>
-                    Selecciona tu foto de perfil
-                    <input
-                      type="file"
-                      id="image-upload"
-                      onChange={handleImageChange}
-                      required
-                      style={{ display: 'none' }}
-                    />
-                 </div>
-                 <br></br>
-                </label>
-                <label className="block">
-                 <input
-                    type="text"
-                    value={nif}
-                    onChange={(e) => setNif(e.target.value)}
-                    required
-                    placeholder='NIF'
-                    className="w-full p-2 mb-4 border border-gray-300 rounded-md"
-                 />
-                </label>
-                <label className="block">
-                 <input
-                    type="tel"
-                    value={phoneNumber}
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                    required
-                    placeholder='Número de teléfono'
-                    className="w-full p-2 mb-4 border border-gray-300 rounded-md"
-                 />
-                </label>
-                <label className="block">
-                 <textarea
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                    placeholder='Descripción'
-                    className="w-full p-2 mb-4 border border-gray-300 rounded-md"
-                 />
-                </label>
-              </div>
-              <div className="w-1/3">
-                <label className="block">
-                 <input
-                    type="checkbox"
-                    checked={isWorker}
-                    onChange={(e) => setIsWorker(e.target.checked)}
-                 />
-                 Soy un trabajador
-                </label>
-                {isWorker && (
-                 <>
-                    <label className="block">
-                      <input
-                        type="text"
-                        value={profession}
-                        onChange={(e) => setProfession(e.target.value)}
-                        placeholder='Profesión'
-                        className="w-full p-2 mb-4 border border-gray-300 rounded-md"
-                      />
-                    </label>
-                    <label className="block">
-                      <input
-                        type="number"
-                        value={experience}
-                        onChange={(e) => setExperience(e.target.value)}
-                        placeholder='Años de experiencia'
-                        className="w-full p-2 mb-4 border border-gray-300 rounded-md"
-                      />
-                    </label>
-                    <label className="block">
-                      <input
-                        type="text"
-                        value={degree}
-                        onChange={(e) => setDegree(e.target.value)}
-                        placeholder='Titulación'
-                        className="w-full p-2 mb-4 border border-gray-300 rounded-md"
-                      />
-                    </label>
-                    <label className="block">
-                      <input
-                        type="text"
-                        value={motherLanguage}
-                        onChange={(e) => setMotherLanguage(e.target.value)}
-                        placeholder='Idioma'
-                        className="w-full p-2 mb-4 border border-gray-300 rounded-md"
-                      />
-                    </label>
-                 </>
-                )}
               </div>
             </div>
             <div className="flex justify-center mt-8">
