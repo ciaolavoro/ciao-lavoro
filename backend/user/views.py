921d6f0d2898ee0468dcb2102b507c61b080d04c
diegonqui <digonquin@gmail.com>
2024-03-10 00:49:53 +0100

    Task 9 - Edicion Perfil de Usuario #121 Arregla la edicion de perfil de usuario, usando SessionAuthentication en lugar de Tokens. Además, crea una funcion update() en users/models.py que permite actualizar el perfil de un usuario sin que su contraseña se resetee y se haga inaccesible, lo cual ocurría antes. Por último, borra validaciones redundantes

--- backend/user/views.py
+++ backend/user/views.py
@@ -9,7 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.contrib.auth.hashers import check_password
 from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.decorators import authentication_classes
 from django.shortcuts import get_object_or_404
 from .serializers import UserSerializer
@@ -69,39 +69,38 @@ class register(APIView):
     
 
 class UserUpdate(APIView):
+   
     def get(self, request, format_arg=None):
-        token_id = request.headers["Authorization"].split()[-1]
-        token = get_object_or_404(Token,key=token_id)
-        user = token.user
-        serializer = UserSerializer(user, many=False)
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+        session_id = request.session.session_key
+        print(session_id)
+        user = request.user
+        serializer = UserSerializer(user)
         return JsonResponse(serializer.data)
     
-    @authentication_classes([TokenAuthentication])
     def put(self, request, format_arg=None):
-        username = request.data.get('username')
+        authentication_classes = [SessionAuthentication]
+        permission_classes = [IsAuthenticated]
+        user = request.user
         first_name = request.data.get('first_name')
         last_name  = request.data.get('last_name')
         email = request.data.get('email')
         language = request.data.get('language')
         birth_date = request.data.get('birth_date')
         image = request.FILES.get('image')
-        token_id = request.headers["Authorization"].split()[-1]
-        token = get_object_or_404(Token,key=token_id)
-        user = token.user
-        if username != '' and username is not None:
-            user.username = username
-        if first_name != '' and first_name is not None:
+        if first_name:
             user.first_name = first_name
-        if last_name != '' and last_name is not None:
+        if last_name:
             user.last_name = last_name
-        if email != '' and email is not None:
+        if email:
             user.email = email
-        if language != '' and language is not None:
+        if language:
             user.language = language
-        if birth_date != '' and birth_date is not None:
+        if birth_date:
             user.birth_date = birth_date
-        if image != '' and image is not None:
+        if image:
             user.image = image
-        user.save()
-        serializer = UserSerializer(user, many=False)
+        user.update()
+        serializer = UserSerializer(user)
         return JsonResponse(serializer.data)
