259743e457e6d0151667eba52b1488b0ad63974d
alvhidrod <alvaro.hidalgo.rodriguez12@gmail.com>
2024-03-10 20:53:29 +0100

    Task 2.23---Intento de rework de contrato---/123

--- backend/contract/urls.py
+++ backend/contract/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from .views import ContractCreation
+from .views import ContractCreation, ContractEdit,ContractStatusEdit,ContractDelete,ContractWorkerList,ContractClientList
 
 app_name = 'contracts'
 
 urlpatterns = [
     path('create/', ContractCreation.as_view(), name='contract-create'),
+    path('edit/<int:contract_id>', ContractEdit.as_view(), name='contract-edit'),
+    path('edit/<int:contract_id>/<int:status_num>', ContractStatusEdit.as_view(), name='contract-status-edit'),
+    path('delete/', ContractDelete.as_view(), name='contract-delete'),
+    path('clientList/', ContractClientList.as_view(), name="client-contracts"),
+    path('workerList/', ContractWorkerList.as_view(), name="worker-contracts")
+
+
 
 ]
