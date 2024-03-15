9aa48de1dda760643c97b51fbf8ea435d09daa8a
Samalbalat <saoalbalat@gmail.com>
2024-03-15 12:33:40 +0100

    Task 2.25 -- Listado de Contratos Filtrado
    Arreglos para Codacy

--- backend/contract/views.py
+++ backend/contract/views.py
@@ -1,5 +1,5 @@
 from datetime import date, datetime
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.decorators import permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
