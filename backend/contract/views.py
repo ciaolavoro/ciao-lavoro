9bdf035d74be6fb0de426cc3e564362e76c0fa47
Samalbalat <saoalbalat@gmail.com>
2024-03-15 13:01:02 +0100

    Task 2.25 - Listado de Contrato Filtrado
    Correción por error de Queryset has no pk

--- backend/contract/views.py
+++ backend/contract/views.py
@@ -109,7 +109,7 @@ class ContractClientList(generics.ListAPIView):
     def get(self, request, *args, **kwargs):
         try:
             queryset = self.get_queryset()
-            serializer = self.serializer_class(queryset, many=True)
+            serializer = self.serializer_class(data= queryset, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
@@ -136,7 +136,7 @@ class ContractWorkerList(generics.ListAPIView):
     def get(self, request, *args, **kwargs):
         try:
             queryset = self.get_queryset()
-            serializer = self.serializer_class(queryset, many=True)
+            serializer = self.serializer_class(data = queryset, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
