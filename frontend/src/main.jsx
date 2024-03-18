11b83e94a1c0cf1e3d0bdc4fdf68c78cba631f83
alisanhos <alexhossdorf@hotmail.com>
2024-03-18 10:16:59 +0100

    Task 2.26 - Reparar listados y llamadas #143

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,9 @@ import Register from './components/register/Register.jsx'
 import UserProfile from './components/user/UserProfile.jsx'
 import Service from './components/service/Service.jsx'
 import { AuthContextProvider } from './components/auth/AuthContextProvider.jsx'
+import Users from './components/user/Users.jsx'
+import ContractUser from './components/contract/ContractUser.jsx'
+import ServiceUser from './components/service/ServiceUser.jsx'
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_API_URL;
 
