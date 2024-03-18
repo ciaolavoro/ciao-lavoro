445498a39d3474afadb39cd5a83b4daaecca0f92
alisanhos <alexhossdorf@hotmail.com>
2024-03-18 13:34:37 +0100

    Task 2.26 - Legibilidad fechas #143

--- frontend/src/components/contract/ContractCardClient.jsx
+++ frontend/src/components/contract/ContractCardClient.jsx
@@ -2,13 +2,18 @@
 
 export function ContractCardClient({ contract }) {
 
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', hour12: false   };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    };
+
     return (
         <a href="#">
             <div className="max-w-md mx-auto my-6 bg-white border rounded-lg overflow-hidden p-6">
                 <h2 className="text-2xl font-semibold text-center">Nombre del Trabajador:</h2>
                 <p className="mb-2 mt-1 text-2xl text-center"><strong>{contract.worker.username}</strong></p>
-                <p className="mb-2"><strong>Fecha de inicio:</strong> {contract.initial_date}</p>
-                <p className="mb-2"><strong>Fecha fin:</strong> {contract.end_date}</p>
+                <p className="mb-2"><strong>Fecha de inicio:</strong> {formatDate(contract.initial_date)}</p>
+                <p className="mb-2"><strong>Fecha fin:</strong> {formatDate(contract.end_date)}</p>
                 <p className="mb-2"><strong>Estado:</strong> {contract.estatus}</p>
             </div>
         </a>
