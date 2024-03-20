156b5b7552d23eb57e79b8c3b28d65c2696ad317
Samalbalat <saoalbalat@gmail.com>
2024-03-20 16:26:55 +0100

    Hotfix 4 - Arreglo errores relacionados con Contrato #232
    Verificaciones de 404, permisos, campos intrínsecos y adición del detalle, todo testeado mediante Thunderclient
    
    - Los contratos nuca se borran de direccion_backend/contracts, no sé porque es exactamente
    - El detalle salta un 400 al buscar un contrato que no existe en lugar del 404 que debería saltar, desconozco el motivo

--- backend/contract/admin.py
+++ backend/contract/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Contract
 
 # Register your models here.
-admin.site.register(Contract)
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = ['client','worker','cost','service','status','initial_date','end_date']
+    search_fields = ['client','worker','cost','initial_date','end_date','status']
--- backend/contract/views.py
+++ backend/contract/views.py
@@ -1,16 +1,16 @@
 from .models import Contract
 from service.models import Service
-from datetime import date, datetime
-from api.serializers import ContractSerializer
+from .serializers import ContractSerializer
 from django.forms import ValidationError
+from datetime import date, datetime
 from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.decorators import authentication_classes
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 class ContractCreation(APIView):
     @authentication_classes([TokenAuthentication])
@@ -21,7 +21,9 @@ class ContractCreation(APIView):
         token_id = request.headers['Authorization']
         token = get_object_or_404(Token, key=token_id.split()[-1])
         client = token.user
-        description = contract_data['description']
+        description = contract_data['description']  
+        if len(description) > 500:
+            raise ValidationError("La descripción no puede superar los 500 caracteres") 
         initial_date = contract_data['initial_date']
         end_date = contract_data['end_date']
         today = datetime.today()
@@ -32,38 +34,60 @@ class ContractCreation(APIView):
         if Init < today:
             raise ValidationError("La fecha de inicio no puede ser antes que hoy")
         cost = float(contract_data['cost'])
-        if cost <= 0:
+        if cost <= 0.0:
             raise ValidationError("El precio no puede ser menor o igual que 0")
         if client == worker:
             raise ValidationError("No puede realizar un contrato a su propio servicio")
-        contract = Contract.objects.create(worker = worker, client = client, description=description,
-                                           initial_date=initial_date,
+        contract = Contract.objects.create(worker = worker, client = client,
+                                           description = description,
+                                           initial_date = initial_date,
                                            end_date = end_date,
                                            cost = cost,
-                                           status=2,
-                                           service=service)
+                                           status = 1,
+                                           service = service)
         serializer = ContractSerializer(contract, many=False,context ={'request': request})
         return Response(serializer.data)
 
 class ContractEdit(APIView):
-    def post(self, request, contract_id):
+    @authentication_classes([TokenAuthentication])
+    def put(self, request, contract_id):
         contract_data = request.data
-        contract = Contract.objects.get(pk=contract_id)
-        if contract.client != request.user and contract.service.user != request.user and not request.user.is_staff:
+        contract = get_object_or_404(Contract, pk = contract_id)
+        token_id = request.headers['Authorization']
+        token = get_object_or_404(Token, key=token_id.split()[-1])
+        user = token.user
+
+        if contract.client != user and contract.service.user != user and not user.is_staff:
             raise ValidationError("No tienes permiso para editar un contrato que no te pertenece")
-        new_accept_worker = contract_data['accept_worker']
-        new_accept_client = contract_data['accept_client']
+        
+        if contract_data['accept_worker']:
+            new_accept_worker = contract_data['accept_worker']
+            if user != contract.worker:
+                raise ValidationError("No tienes permiso para cambiar la aceptación de la otra parte")
+        else:
+            new_accept_worker = contract.accept_worker
+        
+        if contract_data['accept_client']:
+            new_accept_client = contract_data['accept_client']
+            if user != contract.client:
+                raise ValidationError("No tienes permiso para cambiar la aceptación de la otra parte")
+        else:
+            new_accept_client = contract.accept_client
+
         new_description = contract_data['description']
+        if len(new_description) > 500:
+            raise ValidationError("La descripción no puede superar los 500 caracteres")
         new_initial_date = contract_data['initial_date']
         new_end_date = contract_data['end_date']
-        new_cost = contract_data['cost']
-        today = date.today()
-        Init =  datetime.strptime(new_initial_date, '%Y-%m-%d').date()
-        if new_end_date < new_initial_date:
+        today = datetime.today()
+        Init =  datetime.strptime(new_initial_date, '%Y-%m-%dT%H:%M')
+        End = datetime.strptime(new_end_date, '%Y-%m-%dT%H:%M')
+        if End < Init:
             raise ValidationError("La fecha de finalizacion no puede ser antes que la inicial")
         if Init < today:
             raise ValidationError("La fecha de inicio no puede ser antes que hoy")
-        if new_cost <= 0:
+        new_cost = float(contract_data['cost'])
+        if new_cost <= 0.0:
             raise ValidationError("El precio no puede ser menor o igual que 0")
         contract.accept_worker = new_accept_worker
         contract.accept_client = new_accept_client
@@ -76,10 +100,15 @@ class ContractEdit(APIView):
         return Response(serializer.data)
 
 class ContractStatusEdit(APIView):
-    def post(self, request, status_num,contract_id):
-        contract = Contract.objects.get(pk=contract_id)
-        if contract.client != request.user and contract.service.user != request.user and not request.user.is_staff:
-            raise PermissionDenied("No tienes permiso para editar un contrato que no te pertenece")
+    @authentication_classes([TokenAuthentication])
+    def put(self, request, status_num, contract_id):
+        contract = get_object_or_404(Contract, pk = contract_id)
+        token_id = request.headers['Authorization']
+        token = get_object_or_404(Token, key=token_id.split()[-1])
+        user = token.user
+
+        if contract.client != user and contract.service.user != user and not user.is_staff:
+            raise PermissionDenied("No tienes permiso para editar un contrato que no te pertenece") 
         new_status = status_num
         contract.status = new_status
         contract.save()
@@ -87,16 +116,20 @@ class ContractStatusEdit(APIView):
         return Response(serializer.data)
 
 class ContractDelete(APIView):
-    def post(self, request, contract_id):
-        contract = Contract.objects.get(pk=contract_id)
-        if contract.client != request.user and contract.service.user != request.user and not request.user.is_staff:
+    @authentication_classes([TokenAuthentication])
+    def delete(self, request, contract_id):
+        contract = get_object_or_404(Contract, pk = contract_id)
+        token_id = request.headers['Authorization']
+        token = get_object_or_404(Token, key=token_id.split()[-1])
+        user = token.user
+        serializer = ContractSerializer(contract, many=False,context ={'request': request})
+        if contract.client != user and contract.service.user != user and not user.is_staff:
             raise PermissionDenied("No tienes permiso para eliminar un contrato que no te pertenece")
         Contract.delete(contract)
-        serializer = ContractSerializer(contract, many=False,context ={'request': request})
         return Response(serializer.data)
 
 class ContractList(generics.ListAPIView):
-    serializer_class=ContractSerializer
+    serializer_class = ContractSerializer
 
     def get_queryset(self):
         contracts = Contract.objects.all()
@@ -110,24 +143,50 @@ class ContractList(generics.ListAPIView):
             contracts = contracts.filter(initial_date=initial_date)
         if end_date:
             contracts = contracts.filter(end_date=end_date)
-
         return contracts
-
+    
+    @authentication_classes([TokenAuthentication])
     def get(self, request):
         try:
             token_id = self.request.headers['Authorization']
             token = get_object_or_404(Token, key=token_id.split()[-1])
             client = token.user
             queryset = self.get_queryset()
-            queryset1 = queryset.filter(client = client)
-            queryset2 = queryset.filter(worker = client)
+            queryset1 = queryset.filter(worker = client)
+            queryset2 = queryset.filter(client = client)
 
             if not queryset.exists():
                 return Response([], status=status.HTTP_200_OK)
+            
+            serializer1 = self.serializer_class(queryset1, many=True, 
+                                                context ={'request': request})
+            serializer2 = self.serializer_class(queryset2, many=True, 
+                                                context ={'request': request})
 
-            serializer1 = self.serializer_class(queryset1, many=True, context ={'request': request})
-            serializer2 = self.serializer_class(queryset2, many=True, context ={'request': request})
+            return Response({"worker": serializer1.data, "client": serializer2.data},
+                             status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-            return Response({"client": serializer1.data, "worker": serializer2.data}, status=status.HTTP_200_OK)
+class ContractDetail(generics.ListAPIView):
+    serializer_class = ContractSerializer
+    
+    @authentication_classes([TokenAuthentication])
+    def get(self, request, contract_id):
+        try:
+            contract = get_object_or_404(Contract, pk = contract_id)
+            token_id = self.request.headers['Authorization']
+            token = get_object_or_404(Token, key = token_id.split()[-1])
+            user = token.user
+
+            if contract.client != user and contract.service.user != user and not user.is_staff:
+                raise PermissionDenied("No tienes permiso para contemplar un contrato que no te pertenece")
+
+            if not contract != None:
+                return Response([], status=status.HTTP_200_OK)
+            serializer = self.serializer_class(contract, many = False, 
+                                               context ={'request': request})
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
