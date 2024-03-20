6a470816d49e7cd80f9109a6427184679390da91
pabcueper <pabcueper@alum.us.es>
2024-03-20 18:48:00 +0100

    Task 2.45 Primer test y arreglo configuracion #181

--- frontend/src/components/contract/ContractCardWorker.jsx
+++ frontend/src/components/contract/ContractCardWorker.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 export function ContractCardWorker({ contract }) {
 
     const formatDate = (dateString) => {
---
+++ frontend/src/components/contract/ContractTest/ContractCardWorker.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { describe, test, expect } from '@jest/globals';
+import { ContractCardWorker } from '../ContractCardWorker';
+
+describe('ContractCardWorker', () => {
+    test('Username exists', () => {
+        const contractData = {
+            client: { username: 'Nombre de usuario de ejemplo' },
+            initial_date: new Date(),
+            end_date: new Date(),
+            estatus: 'Activo'
+        };
+
+        render(<ContractCardWorker contract={contractData} />);
+
+        const textElement = screen.getByText('Nombre del Cliente:');
+        expect(screen.getByRole('heading')).toBe(textElement);
+    });
+});
