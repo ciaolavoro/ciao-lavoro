25df96e278c9599e4371f595e14f478401c06097
Javier Grosso <javgrogom@alum.us.es>
2024-03-21 11:09:22 +0100

    Fix 2.10 mejora del ojo #219 - Por recomendacion de fran el ojo ahora esta dentro de la contraseña

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { Link, NavLink, Navigate } from "react-router-dom"
-import { loginRequest } from '../../api/login.api'
+import { Link, NavLink, Navigate } from "react-router-dom";
+import { loginRequest } from '../../api/login.api';
 import { useNavigate } from 'react-router-dom';
 import { useAuthContext } from './AuthContextProvider';
 import EyeIcon from '../icons/EyeIcon.jsx';
@@ -11,38 +11,37 @@ export default function LoginPage() {
   const [password, setPassword] = useState('');
   const { login, loggedUser } = useAuthContext();
   const [passwordType, setPasswordType] = useState('password');
-  const [passwordIcon, setPasswordIcon] = useState(EyeSlashIcon);
+  const [passwordIcon, setPasswordIcon] = useState(<EyeSlashIcon />);
   const navigate = useNavigate();
 
-  if(loggedUser) {
-    return (<Navigate to="/" />)
+  if (loggedUser) {
+    return (<Navigate to="/" />);
   }
 
   const loginUser = async (username, password) => {
     try {
-      const res = await loginRequest(username, password)
-      console.log(res.status)
+      const res = await loginRequest(username, password);
+      console.log(res.status);
       if (res.status === '1') {
         login({ user: res.user, token: res.token });
         navigate('/');
-        alert('Se ha iniciado sesión correctamente')
+        alert('Se ha iniciado sesión correctamente');
       } else {
-        alert('Error en el inicio de sesión.  Por favor, verifique su usuario y contraseña.');
+        alert('Error en el inicio de sesión. Por favor, verifique su usuario y contraseña.');
       }
     } catch (error) {
-      if (error)
-      alert('Error en el inicio de sesión.  Por favor, verifique su usuario y contraseña.');
+      alert('Error en el inicio de sesión. Por favor, verifique su usuario y contraseña.');
     }
-  }
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
     loginUser(username, password);
   };
+
   const togglePasswordVisibility = () => {
     setPasswordType(passwordType === 'password' ? 'text' : 'password');
     setPasswordIcon(passwordType === 'password' ? <EyeIcon /> : <EyeSlashIcon />);
-
   };
 
   return (
@@ -59,17 +58,17 @@ export default function LoginPage() {
         <div className='flex flex-col w-full'>
           <label>Contraseña:</label>
           <div className="relative">
-          <input 
-            type={passwordType} 
-            value={password} 
-            placeholder='Contraseña' 
-            onChange={(e) => setPassword(e.target.value)} 
-            required 
-            className="p-2 border border-gray-300 rounded" 
+            <input 
+              type={passwordType} 
+              value={password} 
+              placeholder='Contraseña' 
+              onChange={(e) => setPassword(e.target.value)} 
+              required 
+              className="p-2 border border-gray-300 rounded w-full" // Ajusta según necesidad
             />
-          <span className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer" onClick={() => togglePasswordVisibility()}>
-            {passwordIcon}
-          </span>
+            <span className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer" onClick={togglePasswordVisibility}>
+              {passwordIcon}
+            </span>
           </div>
         </div>
         <button type="submit" className="p-2 bg-orange-400 text-black rounded cursor-pointer font-inherit border-none">Iniciar sesión</button>
