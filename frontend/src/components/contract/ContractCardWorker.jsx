80dd5e1b925cd8cf0f9b46cc1d6618052027dc0e
pabcueper <pabcueper@alum.us.es>
2024-03-22 08:05:17 +0100

    Task 2.45 Ajuste final a la configuracion #181

--- frontend/src/components/contract/ContractCardWorker.jsx
+++ frontend/src/components/contract/ContractCardWorker.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 export function ContractCardWorker({ contract }) {
 
     const formatDate = (dateString) => {
--- frontend/src/components/contract/ContractTest/ContractCardWorker.test.jsx
+++ frontend/src/components/contract/ContractTest/ContractCardWorker.test.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { describe, test, expect } from '@jest/globals';
 import { ContractCardWorker } from '../ContractCardWorker';
