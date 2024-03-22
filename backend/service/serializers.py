b2848a467299cff14f5f34593f434c2437bc3788
fracamcam <fracamcam@alum.us.es>
2024-03-22 10:17:54 +0100

    Task 52: Errores codacy #188

--- backend/service/serializers.py
+++ backend/service/serializers.py
@@ -31,12 +31,12 @@ class ReviewSerializer(serializers.ModelSerializer):
 class ServiceSerializer(serializers.ModelSerializer):
     user = UserSerializer()
     jobs = JobSerializer(many=True, read_only=True, source='job_set')
-    reviews = ReviewSerializer(many=True, read_only=True, source='review_set') 
+    reviews = ReviewSerializer(many=True, read_only=True, source='review_set')
     profession = serializers.SerializerMethodField()
 
     class Meta:
         model = Service
-        fields = ['id', 'user', 'profession', 'city', 'experience', 'is_active', 'is_promoted' , 'jobs', 'reviews'] 
+        fields = ['id', 'user', 'profession', 'city', 'experience', 'is_active', 'is_promoted', 'jobs', 'reviews']
     
     def to_representation(self, instance):
         data = super().to_representation(instance)
