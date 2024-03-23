8dd105f26eef48aeeeaff8cf44325356bc2872ef
alisanhos <alexhossdorf@hotmail.com>
2024-03-23 19:49:35 +0100

    Fix 2.11 - Mejorar Navbar Frontend #218 Cambios hechos en este commit: - Mejorar el navbar responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,14 +8,12 @@ import UserProfileIcon from "./icons/UserProfileIcon"
 import BriefcaseIcon from "./icons/BriefcaseIcon"
 import DocumentIcon from "./icons/DocumentIcon"
 import LogoutIcon from "./icons/LogoutIcon"
-import { useState } from "react"
 
 const navItemsStyle = "px-2 py-1 font-semibold rounded hover:bg-gray-300 transition"
 
 export default function Navbar() {
    const { logout, loggedUser } = useAuthContext()
    const navigate = useNavigate()
-   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
    const navItems = [
       {
@@ -42,7 +40,7 @@ export default function Navbar() {
       {
          id: 2,
          title: "Mis Servicios",
-         icon: <BriefcaseIcon/>,
+         icon: <BriefcaseIcon />,
          path: "/services/user",
       },
       {
@@ -58,6 +56,82 @@ export default function Navbar() {
          logout()
          navigate("/")
       }
+   }
+
+   const renderNabarResponsive = () => {
+      if (loggedUser) {
+         return (
+            <DropdownMenu>
+               <DropdownMenuTrigger>
+                  <li className="rounded-full hover:shadow-lg transition">
+                     <img
+                        src={`${BACKEND_URL}${loggedUser.user.image}` || defaultUserImage}
+                        alt="Imagen de perfil"
+                        className="w-8 h-8 object-cover rounded-full"
+                     />
+                  </li>
+               </DropdownMenuTrigger>
+               <DropdownMenuContent>
+                  {navItems.map(item => (
+                     <Link key={item.id} to={item.path}>
+                        <DropdownMenuItem>
+                           <span className="ml-1">{item.title}</span>
+                        </DropdownMenuItem>
+                     </Link>
+                  ))}
+
+                  {navItemsUser.map(item => (
+                     <Link key={item.id} to={item.path}>
+                        <DropdownMenuItem>
+                           {item.icon}
+                           <span className="ml-1">{item.title}</span>
+                        </DropdownMenuItem>
+                     </Link>
+                  ))}
+                  <DropdownMenuItem onClick={handleLogout}>
+                     <LogoutIcon />
+                     <span className="ml-1">Cerrar sesión</span>
+                  </DropdownMenuItem>
+               </DropdownMenuContent>
+            </DropdownMenu>
+         )
+      } else {
+         return (
+            <DropdownMenu>
+               <DropdownMenuTrigger>
+                  <li className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-300 hover:text-gray-700 hover:border-gray-700 focus:outline-none focus:text-gray-700 focus:border-gray-700">
+                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <title>Menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                     </svg>
+                  </li>
+               </DropdownMenuTrigger>
+               <DropdownMenuContent>
+                  {navItems.map(item => (
+                     <Link key={item.id} to={item.path}>
+                        <DropdownMenuItem>
+                           <span className="ml-1">{item.title}</span>
+                        </DropdownMenuItem>
+                     </Link>
+                  ))}
+                  <Link to="/login">
+                     <DropdownMenuItem>
+                        <span className="ml-1">Iniciar Sesión</span>
+                     </DropdownMenuItem>
+                  </Link>
+
+               </DropdownMenuContent>
+            </DropdownMenu>
+
+
+
+
+            // <NavLink to="/login" className={({ isActive }) => (isActive ? "bg-gray-300 rounded" : "")}>
+            //    <li className={navItemsStyle}>Iniciar sesión</li>
+            // </NavLink>
+         )
+      }
+
    }
 
    const renderLoginOrLogout = () => {
@@ -106,10 +180,15 @@ export default function Navbar() {
                   <img src={ciaoLavoroLogo} alt="Logo de CiaoLavoro" className="w-8 object-cover rounded" />
                </NavLink>
             </section>
+
             <section className="hidden md:flex"> {/* Ocultar en pantallas pequeñas */}
-               <ul className="flex gap-5 py-2">
+               <ul className="flex gap-5 px-4 py-2">
                   {navItems.map((item) => (
-                     <NavLink key={item.id} to={item.path} className={({ isActive }) => isActive ? "bg-gray-300 rounded" : ""}>
+                     <NavLink
+                        key={item.id}
+                        to={item.path}
+                        target={item.out && "_blank"}
+                        className={({ isActive }) => isActive ? "bg-gray-300 rounded" : ""}>
                         <li className={navItemsStyle}>{item.title}</li>
                      </NavLink>
                   ))}
@@ -118,23 +197,10 @@ export default function Navbar() {
             </section>
 
             <section className="md:hidden"> {/* Mostrar solo en pantallas pequeñas */}
-               <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-300 hover:text-gray-700 hover:border-gray-700 focus:outline-none focus:text-gray-700 focus:border-gray-700">
-                  <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-               </button>
-               {isMenuOpen && (
-                  <div className="absolute right-0 w-48 mt-2 py-2 bg-white border rounded shadow-xl">
-                     <ul>
-                        {navItems.map((item) => (
-                           <NavLink key={item.id} to={item.path} className={({ isActive }) => isActive ? "bg-gray-300 rounded" : ""}>
-                              <li className={navItemsStyle}>{item.title}</li>
-                           </NavLink>
-                        ))}
-                        {renderLoginOrLogout()}
-                     </ul>
-                  </div>
-               )}
+               <ul>
+                  {renderNabarResponsive()}
+               </ul>
             </section>
-
 
          </nav>
       </header>
