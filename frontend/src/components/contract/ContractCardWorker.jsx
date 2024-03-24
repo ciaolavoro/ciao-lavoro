add5b87481edac825aea0bb90d57b47ac1f6f959
pabcueper <pabcueper@alum.us.es>
2024-03-24 01:16:25 +0100

    Fix 26 Arreglo de tarjetas de contratos #270

--- frontend/src/components/contract/ContractCardWorker.jsx
+++ frontend/src/components/contract/ContractCardWorker.jsx
@@ -54,12 +54,14 @@ export function ContractCardWorker({ contract }) {
     return (
         <a href="#">
             <div className={`max-w-md mx-auto my-2 border rounded-lg overflow-hidden p-6 ${getStatusColor(contract.estatus)}`}>
-                <h2 className="text-2xl text-center font-semibold">Nombre del Cliente:</h2>
+                <h2 className="text-2xl text-center font-semibold">Nombre del cliente:</h2>
                 <p className="mb-2 mt-1 text-2xl text-center"><strong>{contract.client.username}</strong></p>
                 <p className="mb-2"><strong>Fecha de inicio:</strong> {formatDate(contract.initial_date)}</p>
                 <p className="mb-2"><strong>Fecha fin:</strong> {formatDate(contract.end_date)}</p>
                 <p className={"mb-2"}><strong>Estado:</strong> <p className={`${getStatusColor(contract.estatus)}`}>{contract.estatus}</p></p>
-
+                <p className="mb-2"><strong>Remuneración a recibir:</strong> {contract.cost}€</p>
+                <p className="mb-2"><strong>Descripción del contrato:</strong> {contract.description}</p>
+                
                 {contract.estatus === "Negociacion" && (
                     <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mt-4 mr-3"
                         onClick={() => updateStatus(contract.id, 2, loggedUser.token)}>
