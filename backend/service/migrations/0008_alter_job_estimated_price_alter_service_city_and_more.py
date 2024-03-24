9f3d12871669511e26930c9cba6efa1f436af77e
Joaquín Arregui Díaz <joaquin.arregui2002@gmail.com>
2024-03-24 17:17:53 +0100

    Task 2.60 - Arreglado error codacy#260

--- backend/service/migrations/0008_alter_job_estimated_price_alter_service_city_and_more.py
+++ backend/service/migrations/0008_alter_job_estimated_price_alter_service_city_and_more.py
@@ -14,7 +14,8 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='job',
             name='estimated_price',
-            field=models.DecimalField(decimal_places=2, help_text='Introduzca el coste en euros', max_digits=7, validators=[django.core.validators.MinValueValidator(0.01)]),
+            field=models.DecimalField(decimal_places=2, help_text='Introduzca el coste en euros', 
+                                      max_digits=7, validators=[django.core.validators.MinValueValidator(0.01)]),
         ),
         migrations.AlterField(
             model_name='service',
@@ -24,6 +25,8 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='service',
             name='profession',
-            field=models.IntegerField(choices=[(1, 'Lavandero'), (2, 'Celador'), (3, 'Albañil'), (4, 'Carpintero'), (5, 'Cerrajero'), (6, 'Mecánico'), (7, 'Electricista'), (8, 'Conductor'), (9, 'Pintor'), (10, 'Herrero'), (11, 'Sastre'), (12, 'Profesor particular')]),
+            field=models.IntegerField(choices=[(1, 'Lavandero'), (2, 'Celador'), (3, 'Albañil'), (4, 'Carpintero'), 
+                                               (5, 'Cerrajero'), (6, 'Mecánico'), (7, 'Electricista'), (8, 'Conductor'), 
+                                               (9, 'Pintor'), (10, 'Herrero'), (11, 'Sastre'), (12, 'Profesor particular')]),
         ),
     ]
