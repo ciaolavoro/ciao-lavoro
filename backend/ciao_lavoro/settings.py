838516434dd03fa4af44628daa8c44fefe9d36b7
Joaquín Arregui Díaz <joaquin.arregui2002@gmail.com>
2024-04-07 13:05:47 +0200

    Fix 12 - Aumentar número de request #383
    Aumentado el número de requests de 100 a 1000

--- backend/ciao_lavoro/settings.py
+++ backend/ciao_lavoro/settings.py
@@ -163,8 +163,8 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.UserRateThrottle'
     ],
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
+        'anon': '1000/day',
+        'user': '10000/day'
     }
 }
 
