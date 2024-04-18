ede4a8af8028a7ff83907460844dee76ac255fbd
Javier Grosso <javgrogom@alum.us.es>
2024-04-18 20:36:34 +0200

    Fix 3.28 - Usuario #438 - Arreglado el problema donde cualquier modificacion en el perfil de usuario llevaba a un error 404. Habian eliminado la funcion UserList de backend. Por favor no eliminarla, esa funcion es esencial para el correcto funcionamiento de la edicion de usuario.

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'user'
 
 urlpatterns = [
+    path('',views.UserList.as_view(), name='user-list'),
     path('<int:user_id>/', views.UserDetails.as_view(), name='user-details'),
     path('profile/', views.Profile.as_view(), name='profile'),
     path('register/', views.register.as_view(), name='register'),
